9ed457148240fa4526a2419d953483b112693cda
madankumar3041 <69312892+madankumar3041@users.noreply.github.com>
2021-03-04 13:21:13 +0530

    Add files via upload

---
+++ src/Common/Signup.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import {Navbar} from './Navbar';
+import './Login.css';
+
+export function Signup (){
+    return (
+       <div>
+         <Navbar/>
+          <div class="page-login">
+  <div class="ui centered grid container">
+    <div class="nine wide column">
+      <div class="ui fluid card">
+        <div class="content">
+        <form class="ui form" method="POST">
+          <div class="field">
+            <label>Username</label>
+            <input type="text" name="user" placeholder="Enter Username"/>
+          </div>
+          <div class="field">
+            <label>Email</label>
+            <input type="email" name="email" placeholder="Enter email"/>
+          </div>
+          <div class="field">
+            <label>Password</label>
+            <input type="password" name="pass" placeholder="Password"/>
+          </div>
+          <div class="field">
+            <label>Contact</label>
+            <input type="number" name="contact" placeholder="Optional"/>
+          </div>
+          <button class="ui primary labeled icon button" type="submit">
+            <i class="unlock alternate icon"></i>
+            Signup
+          </button>
+        </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+       </div>
+    );
+}
